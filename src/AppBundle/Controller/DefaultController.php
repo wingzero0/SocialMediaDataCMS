3eefe4a35867b133710d55ee647da612c61da209
wingzero <wingzzz2003@hotmail.com>
2015-06-29 16:48:53 +0800

    connect db table MnemonoBiz

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,22 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/biz", name="biz")
+     */
+    public function getBizAction(){
+    	$bizs = $this->get('doctrine_mongodb')
+	        ->getManager()
+    		->createQueryBuilder('AppBundle:MnemonoBiz')
+    		->getQuery()
+    		->execute();
+
+	    $ret = array();
+	    foreach($bizs as $biz){
+	    	$ret[] = $biz;
+	    }
+    	return new Response(json_encode(array("ret" => $ret)));;
     }
 }
---
+++ src/AppBundle/Document/MnemonoBiz.php
@@ -0,0 +1,80 @@
+<?php
+namespace AppBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document(collection="MnemonoBiz")
+ */
+class MnemonoBiz
+{
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\string
+     */
+    protected $name;
+
+    /**
+     * @MongoDB\collection
+     */
+    protected $urls;
+
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set urls
+     *
+     * @param collection $urls
+     * @return self
+     */
+    public function setUrls($urls)
+    {
+        $this->urls = $urls;
+        return $this;
+    }
+
+    /**
+     * Get urls
+     *
+     * @return collection $urls
+     */
+    public function getUrls()
+    {
+        return $this->urls;
+    }
+}
