720b5d9a9787301d08ada43c139d18fb831a5378
wingzero <wingzzz2003@hotmail.com>
2015-12-09 09:36:18 +0800

    init api bundle

---
+++ src/CodingGuys/ApiBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CodingGuys\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CodingGuysApiBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
