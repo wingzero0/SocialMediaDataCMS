bf964c99d2bf9f1b25d2cbdee84a470cd5718cbc
wingzero <wingzzz2003@hotmail.com>
2016-03-30 20:16:54 +0800

    allow update video link in CMS

--- src/AppBundle/Document/Post.php
+++ src/AppBundle/Document/Post.php
@@ -44,6 +44,7 @@ class Post extends BaseThread{
      */
     protected $tags;
     /**
+     * @deprecated
      * @MongoDB\String
      * @MongoDB\Index
      */
@@ -254,7 +255,7 @@ class Post extends BaseThread{
 
     /**
      * Set mnemonoCat
-     *
+     * @deprecated
      * @param string $mnemonoCat
      * @return self
      */
@@ -266,7 +267,7 @@ class Post extends BaseThread{
 
     /**
      * Get mnemonoCat
-     *
+     * @deprecated
      * @return string $mnemonoCat
      */
     public function getMnemonoCat()
