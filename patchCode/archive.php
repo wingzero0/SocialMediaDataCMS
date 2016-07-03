14c10c495400663b183d341c24e8a3447b068896
wingzero <wingzzz2003@hotmail.com>
2016-07-03 21:12:35 +0800

    use new library to archive data;

--- patchCode/archive.php
+++ patchCode/archive.php
@@ -1,31 +1,91 @@
 <?php
 /**
- * User: kitlei
- * Date: 15-Feb-16
+ * User: kit
+ * Date: 3-Jul-16
  * Time: 9:01 AM
+ *
+ * Archive the post which is 1 months old and it's not the latest 50 feed;
  */
 
-$cli = new \MongoClient();
-$col = $cli->selectCollection("Mnemono", "Post");
+require_once __DIR__ . "/vendor/autoload.php";
+define("MNEMONO", "Mnemono");
+define("MNEMONO_ARCHIVE", "MnemonoArchive");
+define("FACEBOOK_PAGE","FacebookPage");
+define("FACEBOOK_FEED","FacebookFeed");
+define("FACEBOOK_FEED_TIMESTAMP","FacebookFeedTimestamp");
+define("POST","Post");
 
-$testCol = $cli->selectCollection("MnemonoArchive", "Test");
+use MongoDB\Client as MongoDBClient;
+use MongoDB\Collection as MongoDBCollection;
 
-$cursor = $col->find()->sort(array("_id" => 1))->limit(10);
-foreach($cursor as $post){
-    $testCol->insert(array(
-        "manullyRef" => array(
-            "\$ref" => "Post",
-            "\$id" => $post["_id"],
-            "\$db" => "Mnemono",
-            "classPath" => "test",
-        ))
+$cli = new MongoDBClient();
+
+$pageCol = $cli->selectCollection(MNEMONO, FACEBOOK_PAGE);
+$feedCol = $cli->selectCollection(MNEMONO, FACEBOOK_FEED);
+$feedTimestampCol = $cli->selectCollection(MNEMONO, FACEBOOK_FEED_TIMESTAMP);
+$postCol = $cli->selectCollection(MNEMONO, POST);
+
+$nDayAgo = new \DateTime();
+$nDayAgo->setTimezone(new DateTimeZone("GMT"));
+$nDayAgo->sub(new \DateInterval('P30D'));
+
+$archivePageCol = $cli->selectCollection(MNEMONO_ARCHIVE, FACEBOOK_PAGE);
+$archiveFeedCol = $cli->selectCollection(MNEMONO_ARCHIVE, FACEBOOK_FEED);
+$archiveFeedTimestampCol = $cli->selectCollection(MNEMONO_ARCHIVE, FACEBOOK_FEED_TIMESTAMP);
+$archivePostCol = $cli->selectCollection(MNEMONO_ARCHIVE, POST);
+
+$cursor = $pageCol->find();
+foreach ($cursor as $page)
+{
+    upsert($archivePageCol, $page);
+    $id = $page["_id"];
+    $query = [
+        "fbPage.\$id" => $id,
+        "created_time" => ["\$lte" => $nDayAgo->format(\DateTime::ISO8601)]
+    ];
+    $options = [
+        "sort" => ["_id" => -1],
+        "skip" => 25
+    ];
+    $feedCur = $feedCol->find(
+        $query,
+        $options
+    );
+
+    foreach ($feedCur as $feed)
+    {
+        upsert($archiveFeedCol, $feed);
+        deleteOne($feedCol, $feed);
+        $historyCur = $feedTimestampCol->find(
+            ["fbFeed.\$id" => $feed["_id"]]
+        );
+        foreach($historyCur as $timestamp){
+            upsert($archiveFeedTimestampCol, $timestamp);
+            deleteOne($feedTimestampCol, $timestamp);
+        }
+
+        $postCur = $postCol->find(
+            [
+                "importFromRef.\$id" => $feed["_id"],
+                "importFromRef.\$ref" => FACEBOOK_FEED,
+            ]
+        );
+        foreach($postCur as $post){
+            $post["importFromRef"]["\$db"] = MNEMONO_ARCHIVE;
+            upsert($archivePostCol, $post);
+            deleteOne($postCol, $post);
+        }
+    }
+}
+
+function upsert(MongoDBCollection $col, \ArrayObject $oldData){
+    $col->updateOne(
+        ["_id" => $oldData["_id"]],
+        ["\$set" => $oldData],
+        ["upsert" => true]
     );
 }
 
-
-$cursor = $testCol->find()->sort(array("_id" => 1))->limit(10);
-$db = $cli->selectDB("Mnemono");
-foreach($cursor as $manullyRef ){
-    $post = MongoDBRef::get($db, $manullyRef["manullyRef"]);
-    var_dump($post);
+function deleteOne(MongoDBCollection $col, \ArrayObject $oldData){
+    $col->deleteOne(["_id" => $oldData["_id"]]);
 }
